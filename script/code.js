2036567e03d278f0309cdd531b65af065f8b7d42
shakeel <shakeeldvd23@gmail.com>
2024-05-14 13:59:08 +0200

    Exercise

---
+++ script/code.js
@@ -0,0 +1,13 @@
+let firstName = "Shakeel"
+let lastName = "Davids"
+
+console.log(firstName + lastName)
+
+let numberVar = 5
+
+console.log(numberVar * 5)
+// output: 25
+
+const firstName2 = "Shakeel"
+firstName2 = "Shakeel"
+// output: Uncaught TypeError: Assignment to constant variable.
